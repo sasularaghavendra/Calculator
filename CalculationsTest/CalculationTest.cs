764ddaac3242e07d23d6b13e2c569d099ab5ea77
sraghavendra <raghavendrasasula@gmail.com>
2021-02-03 11:08:29 +0530

    Add project files.

---
+++ CalculationsTest/CalculationTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Calculator;
+using Xunit;
+
+namespace CalculationsTest
+{
+    public class CalculationTest
+    {
+        [Fact]
+        public void AddInt_GivenTwoIntValues_ReturnsInt()
+        {
+          
+            var result = Program.addInt(5, 2);
+            Assert.Equal(7,result);
+        }
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Calculator
+{
+    public static class Program
+    {
+        public static  int addInt(int first,int second)
+        {
+            return first + second;
+        }
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
